ac88a6b5849ef9934123355feeaf44baba9908d0
syc1cob <90305902+syc1cob@users.noreply.github.com>
2021-11-05 16:03:28 +0530

    Update printEvenOddNUmbers.cpp

--- printEvenOddNUmbers.cpp
+++ printEvenOddNUmbers.cpp
@@ -23,3 +23,54 @@ int main()
     evenThread.join();
     return 0;
 }
+
+//Using 2 seperate functions
+/*int count =1;
+
+class Base{
+    public:
+    Base(){
+        
+    }
+    void printEven(int end){
+        while(count < end){
+            m1.lock();
+            if(0 == (count&1)){
+                std::cout<<count++<<" "<<std::this_thread::get_id()<<std::endl;
+            }
+            m1.unlock();
+        }
+    }
+    void printOdd(int end){
+        while(count < end){
+            m1.lock();
+            if(count&1){
+                std::cout<<count++<<" "<<std::this_thread::get_id()<<std::endl;
+            }
+            m1.unlock();
+        }
+    }
+};
+
+int main()
+{
+    Base obj;
+    std::thread t1(&Base::printEven, &obj,10);
+    std::thread t2(&Base::printOdd, &obj, 10);
+    t1.join();
+    t2.join();
+
+    return 0;
+}*/
+
+//Output
+/*1 140286979901184
+2 140286988293888
+3 140286979901184
+4 140286988293888
+5 140286979901184
+6 140286988293888
+7 140286979901184
+8 140286988293888
+9 140286979901184
+10 140286988293888*/
